696cdf6382b4839dcf2645446102955d3f0454b6
Alan Casas Arevalo <acasasar@everis.com>
2019-10-26 19:47:15 +0200

    Test should be failed

--- TDDSwiftTests/Models/EuroTests.swift
+++ TDDSwiftTests/Models/EuroTests.swift
@@ -10,25 +10,11 @@ import XCTest
 @testable import TDDSwift
 
 class EuroTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    func testMultiplication () {
+        let euro = Euro(amount: 5)
+        euro.times(times: 2)
+        
+        XCTAssertEqual(euro.amount, 10, "5*2 should be 10")
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
